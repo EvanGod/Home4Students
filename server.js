c519128918ad7d822a1a0bb3d900f14e0d9707bc
EvanGod <2022171006@uteq.edu.mx>
2025-02-21 02:14:27 -0600

    Vercel

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+    res.send("Hola desde Vercel");
+  });
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+
+module.exports = app;
